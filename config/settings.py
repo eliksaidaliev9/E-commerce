4e7331e2e5b0fd4ee46ab6633ed1b2e5346b4bae
Elyor <elik.saidaliev9@gmail.com>
2025-12-22 19:29:07 +0500

    Minor change

--- config/settings.py
+++ config/settings.py
@@ -180,3 +180,4 @@ CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND')
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 SMS_KEY = os.environ.get('SMS_KEY')
+CSRF_TRUSTED_ORIGINS = ["https://*", "http://*"]
