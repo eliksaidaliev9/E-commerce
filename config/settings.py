aad57f81ce2428ef68c5a27aa6143d251115a718
Elyor <elik.saidaliev9@gmail.com>
2026-01-16 23:37:07 +0500

    Update settings

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['elixya.uz', 'www.elixya.uz', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['elixya.uz', 'www.elixya.uz']
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
